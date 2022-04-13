dd7de7e90254370efb3f9f806bc8fe1df6e7592d
Matyi <p.matyi00@gmail.com>
2022-04-13 20:22:52 +0200

    room and user business logic

--- CardsAgaintsHumanityAPI/src/main/java/hu/bme/cah/business_logic/Room.java
+++ CardsAgaintsHumanityAPI/src/main/java/hu/bme/cah/business_logic/Room.java
@@ -1,7 +1,31 @@
-package hu.bme.cah.business_logic.room;
+package hu.bme.cah.business_logic;
 
+import hu.bme.cah.business_logic.User;
 
 public class Room {
 
+    private List<User> allowedUsers;
+    private List<User> connectedUsers;
+
+    public void startGame(){
+
+    }
+
+    public void joinUser(User u)
+    {
+        if (allowedUsers.contains(u))
+        {
+            u.setRoom(this);        //ez fogja meghívni, vagy a controller?
+
+            if (connectedUsers.contains(u) == false)
+                connectedUsers.append(u);
+        }
+    }
+
+    public void leaveUser(User u)
+    {
+        if (connectedUsers.contains(u) == true)
+                connectedUsers.delete(u);   //nem biztos hogy ez a függvény jó
+    }
 
 }
--- CardsAgaintsHumanityAPI/src/main/java/hu/bme/cah/business_logic/User.java
+++ CardsAgaintsHumanityAPI/src/main/java/hu/bme/cah/business_logic/User.java
@@ -1,6 +1,41 @@
-package hu.bme.cah.business_logic.user;
+package hu.bme.cah.business_logic;
+
+import hu.bme.cah.business_logic.Room;
 
 public class User {
+
+    private Room currentRoom;
+    private boolean isCzar;
+
+    private Game game;
+
+    private List<WhiteCard> ownedWhiteCards;
+    private BlackCard blackCard;
+    private List<List<WhiteCard>> pickedWhiteCards;
+
+    public void setRoom(Room r)
+    {
+        currentRoom = r;
+    }
+
+    public boolean getIsCzar(){
+        return  isCzar;
+    }
+
+    public boolean setGame(Game g)
+    {
+        game = g;
+    }
+
+    public void voteOnCard(int a)
+    {
+        game.vote(pickedWhiteCards[a]);     //valahogy így
+    }
+
+    public void pickWhiteCard(int a)
+    {
+        game.pickWhiteCard(ownedWhiteCards[a]);
+    }
 
 
 }
