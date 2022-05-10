489a59d6045a5dc7c9b337d790c7bdb789a7e806
Matyi00 <p.matyi00@gmail.com>
2022-05-10 21:22:16 +0200

    Update networkcontroller.h

--- CardsAgainstHumanity/networkcontroller.h
+++ CardsAgainstHumanity/networkcontroller.h
@@ -2,6 +2,11 @@
 #define NETWORKCONTROLLER_H
 
 #include <QObject>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
 
 class NetworkController : public QObject
 {
@@ -9,7 +14,11 @@ class NetworkController : public QObject
 public:
     explicit NetworkController(QObject *parent = nullptr);
 
-
+private:
+    QNetworkAccessManager* manager;
+    QString loggedInUsername;
+    QString loggedInPassword;
+    QString joinedRoomId;
 
 signals:
     void successfullyLoggedIn();
@@ -20,26 +29,38 @@ signals:
     void connectionLost();
     void gameStarted();
     void gameState(QString state, int round, int maxrounds);
-    void cardsReceived(QString blackCard, QStringList cards);
-    void picksReceived(QStringList picks, QStringList users);
+    void cardsReceived(QString blackCard, QStringList cards, int picks);
+    void picksReceived(QStringList picks, QStringList users, int numberOfPicks);
 
 
 public slots:
-    void login(QString uname, QString pw);
-    void registeruser(QString uname, QString email, QString password);
-    void createRoom(QString name);
-    void joinRoom(QString room);
-    void leaveRoom();
-    void startGame(int rounds);
-    void kickPlayer(QString name);
+    void login(QString uname, QString pw);//
+    void registeruser(QString uname, QString email, QString password);//
+    void createRoom(QString name);//
+    void joinRoom(QString room);//
+    void leaveRoom();//
+    void startGame(int rounds);//
+    void kickPlayer(QString name);//???
     void invitePlayer(QString name);
     void getRoomData();
-    void getRoomList();
+    void getRoomList();//
     void sendPickedCards(QStringList cards);
     void sendVote(QString vote);
     void updateGameState();
     void getCards();
     void getPicks();
+
+    void handleLoginResult(QNetworkReply *reply);
+    void handleRegisterResult(QNetworkReply *reply);
+    void handleRoomListResult(QNetworkReply *reply);
+    void handleRoomCreateResult(QNetworkReply *reply);
+    void handleRoomJoinResult(QNetworkReply *reply);
+    void handleRoomLeaveResult(QNetworkReply *reply);
+    void handleGameStartResult(QNetworkReply *reply);
+    void handlePlayerKickResult(QNetworkReply *reply);
+    void handleRoomDataResult(QNetworkReply *reply);
+
+
 };
 
 #endif // NETWORKCONTROLLER_H
